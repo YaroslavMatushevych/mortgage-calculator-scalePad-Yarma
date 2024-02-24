7c8dbc000603d17dea91ff3a15a786bb63737aa3
ymatushevych-appwell <ymatushevych-appwell@ancestry.com>
2024-02-23 17:00:03 -0800

    chore: updated types

--- src/types/validationSchemas.ts
+++ src/types/validationSchemas.ts
@@ -12,7 +12,8 @@ export const mortgageCalculatorSchema = z.object({
   annual_interest_rate: z
     .number()
     .positive('Interest rate must be positive')
-    .min(0.1, 'Annual interest rate is required'),
+    .min(0.1, 'Annual interest rate is required')
+    .max(100, 'Annual interest rate is max 100%'),
   amortization_period: z.number().int().min(5).max(30).step(5),
-  payment_schedule: z.enum(['accelerated bi-weekly', 'bi-weekly', 'monthly']),
+  payment_schedule: z.enum(['accel-bi-weekly', 'bi-weekly', 'monthly']),
 });
