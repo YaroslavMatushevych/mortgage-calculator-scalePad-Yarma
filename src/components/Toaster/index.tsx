988a9a6fd7533051625a844239102e8bb300a483
ymatushevych-appwell <ymatushevych-appwell@ancestry.com>
2024-02-23 17:01:26 -0800

    feat: added component Toast

---
+++ src/components/Toaster/index.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import React from 'react';
+import {
+  ToastContainer,
+  toast,
+  Bounce,
+  ToastContainerProps,
+} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const invokeToastNotification = (error: string) => {
+  return toast.error(error, {
+    position: 'top-right',
+    autoClose: 4500,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'colored',
+    transition: Bounce,
+  });
+};
+
+const ToastContainerComponent: React.FC<ToastContainerProps> = ({
+  position = 'top-right',
+  autoClose = 5000,
+  hideProgressBar = false,
+  newestOnTop = false,
+  closeOnClick = true,
+  rtl = false,
+  pauseOnFocusLoss = true,
+  draggable = true,
+  pauseOnHover = true,
+  transition: Bounce,
+}) => {
+  return (
+    <ToastContainer
+      position={position}
+      autoClose={autoClose}
+      hideProgressBar={hideProgressBar}
+      newestOnTop={newestOnTop}
+      closeOnClick={closeOnClick}
+      rtl={rtl}
+      pauseOnFocusLoss={pauseOnFocusLoss}
+      draggable={draggable}
+      pauseOnHover={pauseOnHover}
+      transition={Bounce}
+    />
+  );
+};
+
+export default ToastContainerComponent;
