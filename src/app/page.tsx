26a6345ce46fafee8d109b71b946ead7716b0ff8
ymatushevych-appwell <ymatushevych-appwell@ancestry.com>
2024-02-23 17:03:04 -0800

    feat: updated mane page view and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,33 @@
-'use client';
-
-import Head from 'next/head';
+import { Metadata } from 'next';
 import React from 'react';
+
+import styles from './page.module.scss';
+
+import MortgageCalculator from '@/components/MortgageCalculator';
+
+export const metadata: Metadata = {
+  title: 'Mortgage Calculator',
+  description:
+    'Mortgage Calculator you can calculate your mortgage in BC with mortcalc.ca',
+};
 
 export default function HomePage() {
   return (
-    <main>
-      <Head>
-        <title>Hi</title>
-      </Head>
-      <section className='bg-white'></section>
+    <main className={styles.main}>
+      <section className={styles.section}>
+        <h1 className={styles.title}>BC CMHC Insurance</h1>
+        <p className={styles.description}>
+          In Canada, homebuyers with less than 20% down payment require mortgage
+          default insurance, or CMHC insurance. Mortgage default insurance
+          protects lenders in the event a home buyer defaults. Thanks to this
+          protection, lenders can offer lower mortgage rates for mortgages with
+          lower down payments.
+        </p>
+        <div className={styles.formTitle}>
+          <h2>Mortcalc.ca's BC CMHC calculator</h2>
+        </div>
+        <MortgageCalculator />
+      </section>
     </main>
   );
 }
